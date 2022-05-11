85a6f7afb3ff3267aa23e79c8a424ce91f8ee39e
michaelchangdk <michaelchangdk@icloud.com>
2022-05-11 22:11:50 +0200

    adding google authentication

--- web/src/pages/login/Signup.jsx
+++ web/src/pages/login/Signup.jsx
@@ -1,12 +1,38 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "../AuthProvider";
-import { auth } from "../../firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, provider } from "../../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 const Signup = () => {
   const { setUser, setUserid } = useContext(AuthContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const googleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
 
   const register = () => {
     createUserWithEmailAndPassword(auth, email, password)
