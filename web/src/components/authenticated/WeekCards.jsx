3a6f5db62086d683cc77bb742889cd835d664db3
michaelchangdk <michaelchangdk@icloud.com>
2022-05-26 22:12:11 +0200

    working on the progress circles on the week overview page

--- web/src/components/authenticated/WeekCards.jsx
+++ web/src/components/authenticated/WeekCards.jsx
@@ -4,32 +4,75 @@ import { Card, Stack, Typography, Button } from "@mui/material";
 import ProgressCircle from "./ProgressCircle";
 import { useSelector } from "react-redux";
 import NoAccessModal from "./NoAccessModal";
+import { client } from "../../client";
 // import StartRoundedIcon from "@mui/icons-material/StartRounded";
 
 const WeekCards = ({
+  name,
   title,
   keyword,
   shortDescription,
   liveSessionTitle,
   liveSessionDate,
-  name,
+  module,
 }) => {
+  const navigate = useNavigate();
   const [progress, setProgress] = useState(0);
   const [openModal, setOpenModal] = useState(false);
   const [disabled, setDisabled] = useState(false);
-  const navigate = useNavigate();
   const access = Object.entries(
     useSelector((store) => store.authenticated.access)
   ).filter(([key, val]) => key.includes(name) && val === true);
+  const moduleQueries = module.map(
+    (module) => `*[_type == "module" && _id == "${module._ref}"] {lesson}`
+  );
+  const [lessonRefArray, setLessonRefArray] = useState([]);
 
-  const progressTracker = () => {
-    setProgress(0);
+  // THIS IS FOR THE PROGRESS BARS - BUGGY //
+  let completed = useSelector(
+    (store) => store.authenticated.completedLessons
+  ).map((lesson) => lesson.lessonRef);
+  let counter = 0;
+
+  const fetchLessonRefs = () => {
+    // eslint-disable-next-line array-callback-return
+    moduleQueries.map((query) => {
+      client.fetch(query).then((response) => {
+        const lessonRefs = response[0].lesson.map((lesson) => lesson._ref);
+        lessonRefs.forEach((ref) => console.log(ref));
+        lessonRefs.forEach((ref) =>
+          setLessonRefArray((prev) => [...prev, ref])
+        );
+      });
+    });
+  };
+
+  // I THINK THE ISSUE IS THIS FUNCTION AND WHEN IT GETS CALLED
+  const compareArrays = (array1, array2) => {
+    array1.forEach((element) => {
+      if (array2.includes(element)) {
+        counter++;
+      }
+    });
   };
 
   useEffect(() => {
-    progressTracker();
+    fetchLessonRefs();
+    compareArrays(completed, lessonRefArray);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // const progressTracker = () => {
+  //   setProgress((counter / lessonRefArray.length) * 100);
+  // };
+
+  useEffect(() => {
+    // progressTracker();
+    setProgress((counter / lessonRefArray.length) * 100);
+  }, [counter, lessonRefArray.length]);
+  // PROGRESS BAR SECTION OVER
+
+  // NAVIGATION AND ACCESS //
   const navigateToWeek = () => {
     if (access.length === 1) {
       navigate(`/week/${name}`);
@@ -43,6 +86,7 @@ const WeekCards = ({
       setDisabled(true);
     }
   }, [access.length]);
+  // NAVIGATION AND ACCESS //
 
   return (
     <Card
@@ -54,10 +98,8 @@ const WeekCards = ({
         mx: "auto",
       }}
     >
-      <Stack
-        sx={{ height: "100%", justifyContent: "space-between" }}
-        height="100%"
-      >
+      {/* Is height necessary below? */}
+      <Stack sx={{ height: "100%", justifyContent: "space-between" }}>
         <Stack
           direction="row"
           alignItems="center"
--- web/src/pages/authenticated/WeekOverview.jsx
+++ web/src/pages/authenticated/WeekOverview.jsx
@@ -4,34 +4,21 @@ import { client } from "../../client";
 import WeekCards from "../../components/authenticated/WeekCards";
 import LandingPageHero from "../../components/authenticated/LandingPageHero";
 import { Box } from "@mui/material";
-// import { useSelector } from "react-redux";
 
 const WeekOverview = () => {
   const [loading, setLoading] = useState(true);
   const [cards, setCards] = useState(null);
 
-  // THIS IS GOING TO BE USED FOR THE PROGRESS BARS
-  // let completed = useSelector((store) => store.authenticated.completedLessons);
-  // let user = useSelector((store) => store.authenticated.uid)
-
-  const weekQuery = '*[_type == "week"] ';
-  // { order, name, keyword, shortDescription, title, subtitle, liveSessionTitle, liveSessionDate, _id}
-
-  const fetchWeek = async () => {
-    const fetch = await client.fetch(weekQuery);
-    const response = await fetch;
-    // NEED BELOW CONSOLE LOG FOR TRACKING PROGRESS - MAYBE ON WEEK CARD INSTEAD??
-    // console.log(response);
-    // console.log(response.map((lesson) => lesson.module));
-
-    // WE NEED TO MAKE SURE DRAFTS DON'T GET ADDED TO THE ARRAY! USE THE BELOW CODE IN IDS!
-    let published = response.filter((a) => !a._id.includes("draft"));
-    setCards(published.sort((a, b) => a.order - b.order));
-    setLoading(false);
-  };
+  // For setting the week cards
+  const weekQuery =
+    '*[_type == "week"] {order, name, keyword, shortDescription, title, subtitle, liveSessionTitle, liveSessionDate, _id, module}';
 
   useEffect(() => {
-    fetchWeek();
+    client.fetch(weekQuery).then((response) => {
+      let published = response.filter((a) => !a._id.includes("draft"));
+      setCards(published.sort((a, b) => a.order - b.order));
+      setLoading(false);
+    });
   }, []);
 
   return (
@@ -60,6 +47,7 @@ const WeekOverview = () => {
               shortDescription={week.shortDescription}
               liveSessionTitle={week.liveSessionTitle}
               liveSessionDate={week.liveSessionDate}
+              module={week.module}
             />
           ))}
       </PageContainer>
