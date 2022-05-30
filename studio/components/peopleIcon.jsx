8c716ce7e44791588c100fb5e536b7083b65cdc7
michaelchangdk <michaelchangdk@icloud.com>
2022-05-30 17:52:44 +0200

    removed props from icon components

--- studio/components/peopleIcon.jsx
+++ studio/components/peopleIcon.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import icon from "../assets/people_pink.svg";
 
-const peopleIcon = (props) => {
+const peopleIcon = () => {
   return (
     <>
       <img
         src={icon}
         alt="school icon"
         style={{ width: "35px", height: "35px" }}
-        props={props}
       />
     </>
   );
--- studio/components/privateIcon.jsx
+++ studio/components/privateIcon.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import icon from "../assets/private_pink.svg";
 
-const privateIcon = (props) => {
+const privateIcon = () => {
   return (
     <>
       <img
         src={icon}
         alt="school icon"
         style={{ width: "35px", height: "35px" }}
-        props={props}
       />
     </>
   );
