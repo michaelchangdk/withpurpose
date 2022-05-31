8a838d347da9d76a554235b7106fc3e2cc7e7133
Amanda Tange <amandatange@hotmail.com>
2022-05-31 17:01:47 +0200

    implemented possibility to change email

--- web/src/pages/authenticated/ProfilePage.jsx
+++ web/src/pages/authenticated/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import HeaderAuth from "../../components/authenticated/HeaderAuth";
-import { getAuth, updateProfile, updateEmail, sendPasswordResetEmail  } from "firebase/auth";
+import imageUrlBuilder from '@sanity/image-url';
+import { EmailAuthProvider, getAuth, updateProfile, updateEmail, sendPasswordResetEmail, reauthenticateWithCredential  } from "firebase/auth";
 import { useDispatch, useSelector } from "react-redux";
 import { authenticated } from "../../reducers/authenticated";
 import {
@@ -26,14 +27,17 @@ import { Box } from "@mui/material";
 const ProfilePage = () => {
   const [checked, setChecked] = useState(false);
   const [filename, setFilename] = useState('choose image');
+  const [imageFile, setImageFile] = useState({});
   const [firstname, setFirstname] = useState('');
   const [lastname, setLastname] = useState('');
   const [currentEmail, setCurrentEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [newEmail, setNewEmail] = useState('');
   const [confirmNewEmail, setConfirmNewEmail] = useState('');
   const [error, setError] = useState("");
 
   const auth = getAuth();
+  const builder = imageUrlBuilder(client);
   const dispatch = useDispatch();
   const userid = useSelector((store) => store.authenticated.uid);
   const darkMode = useSelector((store) => store.authenticated.darkMode);
@@ -43,6 +47,11 @@ const ProfilePage = () => {
 
   const emailPattern =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    const urlFor = (source) => {
+      
+      return builder.image(source)
+    }
 
   const toggleDarkMode = () => {
     if (checked) {
@@ -64,13 +73,37 @@ const ProfilePage = () => {
     };
   };
 
+  const updateAvatar = () => {
+    
+    
+    const photoURL = urlFor(imageFile)
+
+    // client
+    //   .patch(userid)
+    //   .set({
+    //     avatar: imageFile
+    //   })
+    //   // .commit({ autoGenerateArrayKeys: true })
+    //   .commit()
+    //   .then((result) => {
+    //     
+    //     // dispatch(authenticated.actions.changeDisplayname(`${firstname} ${lastname}`));
+    //   })
+  //   curl \
+  // -X POST \
+  // -H 'Content-Type: image/jpeg' \
+  // --data-binary "@/Users/mike/images/bicycle.jpg" \
+  // 'https://myProjectId.api.sanity.io/v2021-06-07/assets/images/myDataset'
+
+  }
+
   const updateDisplayName = () => {
-    // console.log(firstname, lastname)
+    // 
     if (firstname.length < 2 || lastname.length < 2) {
       setError("Please fill out your name.");
     } else {
       setError('');
-      updateProfile(auth.currentUser, {... auth.currentUser,
+      updateProfile(auth.currentUser, {...auth.currentUser,
         displayName: `${firstname} ${lastname}`
       }).then(() => {
         // Profile updated!
@@ -88,7 +121,6 @@ const ProfilePage = () => {
         // .commit({ autoGenerateArrayKeys: true })
         .commit()
         .then((result) => {
-          console.log(result);
           dispatch(authenticated.actions.changeDisplayname(`${firstname} ${lastname}`));
         })
     }
@@ -98,6 +130,7 @@ const ProfilePage = () => {
     sendPasswordResetEmail(auth, currentEmail)
     .then(() => {
       // Password reset email sent!
+      // give user confirmation that the email was sent, and suggest looking in spam too
       // ..
     })
     .catch((error) => {
@@ -108,22 +141,43 @@ const ProfilePage = () => {
     });
   }
 
-  const updateEmail = () => {
-    // console.log(newEmail)
+  const updateEmailAddress = () => {
+    // 
     if (newEmail !== confirmNewEmail) {
       setError("Emails do not match.");
     } else if (!newEmail.match(emailPattern)) {
       setError("Please enter a valid email address.");
     } else {
       setError('');
-      updateEmail(auth.currentUser, newEmail).then(() => {
-        // Email updated!
-        // ...
+      // 
+      let credential = EmailAuthProvider.credential(
+        auth.currentUser.email,
+        password
+      );
+
+      
+      reauthenticateWithCredential(auth.currentUser, credential)
+      .then(result => {
+      // User successfully reauthenticated. New ID tokens should be valid.
+        updateEmail(auth.currentUser, newEmail).then(() => {
+          
+
+          //Add it to client tooooo
+
+          client
+            .patch(userid)
+            .set({
+              email: newEmail
+            })
+            // .commit({ autoGenerateArrayKeys: true })
+            .commit()
+            .then((data) => {
+              
+            })
       }).catch((error) => {
-        // An error occurred
-        // ...
+        
       });
-      // call on client and firebase to update email?
+    })
     }
   };
 
@@ -178,12 +232,15 @@ const ProfilePage = () => {
             <AccordionDetails>
               <Input
               accept="image/*"
-              // className={classes.input}
               style={{ display: 'none' }}
               id="raised-button-file"
               multiple
               type="file"
-              onChange={(event) => setFilename(event.target.value)}
+              onChange={(event) => {
+                setFilename(event.target.files[0].name)
+                setImageFile(event.target.files[0])
+                }
+              }
             />
             <label htmlFor="raised-button-file">
               <Button variant="raised" component="span" 
@@ -191,7 +248,7 @@ const ProfilePage = () => {
               {filename}
               </Button>
             </label>
-            <Button>
+            <Button onClick={() => updateAvatar()}>
               Upload
             </Button>
             </AccordionDetails>
@@ -228,43 +285,43 @@ const ProfilePage = () => {
                 Change email
               </Typography>
             </AccordionSummary>
-            <AccordionDetails>
-              {/* <TextField
-              label="Current email address"
-              variant="outlined"
-              fullWidth
-              required={true}
-              // onChange={(e) => setEmail(e.target.value)}
-              // autoComplete="Email"
-              > 
-              </TextField> */}
+            <AccordionDetails>  
               <TextField
-              label="New email address"
-              variant="outlined"
-              fullWidth
-              required={true}
-              onChange={(e) => setNewEmail(e.target.value)}
-              // autoComplete="Email"
-              >
-              
-              </TextField>
+                label="Current email address"
+                variant="outlined"
+                fullWidth
+                required={true}
+                onChange={(e) => setCurrentEmail(e.target.value)}
+                />
               <TextField
-              label="Confirm new email"
-              variant="outlined"
-              fullWidth
-              required={true}
-              onChange={(e) => setConfirmNewEmail(e.target.value)}
-              // autoComplete="Email"
-              >
-              
-              </TextField>
+                label="Password"
+                variant="outlined"
+                type="password"
+                required={true}
+                fullWidth
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <TextField
+                label="New email address"
+                variant="outlined"
+                fullWidth
+                required={true}
+                onChange={(e) => setNewEmail(e.target.value)}
+              />
+              <TextField
+                label="Confirm new email"
+                variant="outlined"
+                fullWidth
+                required={true}
+                onChange={(e) => setConfirmNewEmail(e.target.value)}
+              />
               {error.length > 0 && (
                 <Alert severity="warning">
                   <AlertTitle>Error</AlertTitle>
                   {error}
                 </Alert>
               )}
-              <Button onClick={updateEmail}>
+              <Button onClick={updateEmailAddress}>
                 Change email
               </Button>
             </AccordionDetails>
