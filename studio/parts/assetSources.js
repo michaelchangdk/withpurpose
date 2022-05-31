ba14cb88031262085998c3902f629411d792930d
michaelchangdk <michaelchangdk@icloud.com>
2022-05-31 16:14:11 +0200

    uninstalled sanity media and installed sanity media-library instead

--- studio/parts/assetSources.js
+++ studio/parts/assetSources.js
@@ -1,4 +1,4 @@
-import MediaAssetSource from "part:sanity-plugin-media/asset-source";
 import Unsplash from "part:sanity-plugin-asset-source-unsplash/image-asset-source";
+import AssetSource from "part:sanity-plugin-media-library/asset-source";
 
-export default [MediaAssetSource, Unsplash];
+export default [Unsplash, AssetSource];
