e82820e80c47e7c781ee0510b094c7dab935d39b
michaelchangdk <michaelchangdk@icloud.com>
2022-06-01 14:10:02 +0200

    styling landing page cards

--- web/src/components/authenticated/LandingCards.jsx
+++ web/src/components/authenticated/LandingCards.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-// import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import NoAccessModal from "./NoAccessModal";
+import { urlFor } from "../../client";
+import styled from "styled-components";
 
-const LandingCards = ({ title, headline, description, linkTo }) => {
+const LandingCards = ({ title, headline, description, linkTo, coverImage }) => {
   const access = useSelector((store) => store.authenticated.access);
   const navigate = useNavigate();
   const [openModal, setOpenModal] = useState(false);
+  const coverImageURL = coverImage.asset._ref;
 
   // FEEL LIKE THIS IF STATEMENT CAN BE SOLVED IN A BETTER WAY VIA NAMING CONVENTIONS?
   const clickCard = () => {
@@ -33,19 +35,19 @@ const LandingCards = ({ title, headline, description, linkTo }) => {
 
   return (
     <Card
-      sx={
-        {
-          // maxWidth: 345
-        }
-      }
+      sx={{
+        width: "100%",
+        maxWidth: 500,
+      }}
     >
       <CardActionArea onClick={clickCard}>
-        {/* <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        /> */}
+        <AspectRatioBox>
+          <AspectRatioChild
+            backgroundimage={urlFor(coverImageURL).url()}
+            xposition={coverImage.crop.top}
+            yposition={coverImage.hotspot.y}
+          ></AspectRatioChild>
+        </AspectRatioBox>
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {title}
@@ -64,3 +66,23 @@ const LandingCards = ({ title, headline, description, linkTo }) => {
 };
 
 export default LandingCards;
+
+const AspectRatioBox = styled.div`
+  height: 0;
+  overflow: hidden;
+  padding-top: 56.25%;
+  position: relative;
+`;
+
+const AspectRatioChild = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.backgroundimage});
+  background-position-x: ${(props) => props.xposition * 100}%;
+  background-position-y: ${(props) => props.yposition * 100}%;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
--- web/src/pages/authenticated/LandingPage.jsx
+++ web/src/pages/authenticated/LandingPage.jsx
@@ -4,12 +4,13 @@ import LandingCards from "../../components/authenticated/LandingCards";
 import { PageContainer } from "../../styledcomponents/globalstyles";
 import { Box } from "@mui/material";
 import LandingPageHero from "../../components/authenticated/LandingPageHero";
+import styled from "styled-components";
 
 const LandingPage = () => {
   const [loading, setLoading] = useState(true);
   const [cards, setCards] = useState(null);
   const query =
-    '*[_type == "landingpageelements"] {order, title, headline, description, linkTo}';
+    '*[_type == "landingpageelements"] {order, title, headline, description, linkTo, coverImage}';
 
   useEffect(() => {
     client.fetch(query).then((response) => {
@@ -34,19 +35,41 @@ const LandingPage = () => {
         displayName={true}
       />
       <PageContainer>
-        {!loading &&
-          cards.map((card) => (
-            <LandingCards
-              key={card.order}
-              title={card.title}
-              headline={card.headline}
-              description={card.description}
-              linkTo={card.linkTo}
-            />
-          ))}
+        <CardGrid>
+          {!loading &&
+            cards.map((card) => (
+              <LandingCards
+                key={card.order}
+                title={card.title}
+                headline={card.headline}
+                description={card.description}
+                linkTo={card.linkTo}
+                coverImage={card.coverImage}
+              />
+            ))}
+        </CardGrid>
       </PageContainer>
     </Box>
   );
 };
 
 export default LandingPage;
+
+const CardGrid = styled.div`
+  display: grid;
+  /* justify-content: center;
+  align-items: center; */
+  gap: 2vh;
+  padding: 2vh 0;
+  margin: 0 auto;
+  max-width: 500px;
+
+  @media (min-width: 768px) {
+    max-width: calc(1000px + 3vh);
+    /* grid-template-columns: 500px 500px; */
+    grid-template-columns: 1fr 1fr;
+    gap: 3vh;
+    padding: 3vh 0;
+    justify-items: center;
+  }
+`;
