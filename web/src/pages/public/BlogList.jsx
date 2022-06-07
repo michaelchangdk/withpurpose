bc4e6b8d3176018e9d055941629f8bbbc4f620de
Amanda Tange <amandatange@hotmail.com>
2022-06-07 19:39:15 +0200

    added second fetch for individual blog post

--- web/src/pages/public/BlogList.jsx
+++ web/src/pages/public/BlogList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PublicHeader from "../../components/public/PublicHeader";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { darkMode } from "../../styledcomponents/themeoptions";
 
@@ -13,17 +13,26 @@ import {PortableText} from '@portabletext/react'
 const BlogList = () => {
   // const [loading, setLoading] = useState(true);
   const [blogposts, setBlogposts] = useState([]);
+  const [currentPost, setCurrentPost] = useState(null);
   
 
   const fetchBlogposts = async () => {
     // setLoading(true);
-    const blogpostQuery = `*[_type == "blogpost"]`;
+    const blogpostQuery = `*[_type == "blogpost"] {_id, title}`;
     const fetch = await client.fetch(blogpostQuery);
     const response = await fetch;
     setBlogposts(response);
+    console.log(response);
     // console.log(response[0])
     // setLoading(false);
   };
+
+  const fetchPost = async (id) => {
+    const postQuery = `*[_type == "blogpost" && _id == '${id}']`;
+    const fetch = await client.fetch(postQuery);
+    const response = await fetch;
+    setCurrentPost(response[0]);
+  }
 
   useEffect(() => {
     fetchBlogposts();
@@ -44,14 +53,22 @@ const BlogList = () => {
         Blog Posts
         {blogposts.map((blogpost) => {
           return (
-            <PortableText
-              key={blogpost._id}
-              value={blogpost.body}
-            />
+            // <PortableText
+            //   key={blogpost._id}
+            //   value={blogpost.title}
+            // />
+            <button key={blogpost._id} onClick={() => fetchPost(blogpost._id)}>
+              <Typography>{blogpost.title}</Typography>
+            </button>
+            
           )
         })
-          
+        
         }
+        <PortableText
+          value={currentPost?.body}
+        />
+        
       </Box>
     </ThemeProvider>
   );
