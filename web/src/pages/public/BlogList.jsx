f89aa4feab41e42a70ca365cc2daf3c13e21d489
Amanda Tange <amandatange@hotmail.com>
2022-06-07 13:46:03 +0200

    fetching all blogposts and mapping them showing the whole post

--- web/src/pages/public/BlogList.jsx
+++ web/src/pages/public/BlogList.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PublicHeader from "../../components/public/PublicHeader";
 import { Box } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { darkMode } from "../../styledcomponents/themeoptions";
 
+import { client } from "../../client";
+
+import {PortableText} from '@portabletext/react'
+
+
+
 const BlogList = () => {
+  // const [loading, setLoading] = useState(true);
+  const [blogposts, setBlogposts] = useState([]);
+  
+
+  const fetchBlogposts = async () => {
+    // setLoading(true);
+    const blogpostQuery = `*[_type == "blogpost"]`;
+    const fetch = await client.fetch(blogpostQuery);
+    const response = await fetch;
+    setBlogposts(response);
+    // console.log(response[0])
+    // setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchBlogposts();
+  }, []);
+
   return (
     <ThemeProvider theme={darkMode}>
       <Box
@@ -18,6 +42,16 @@ const BlogList = () => {
       >
         <PublicHeader />
         Blog Posts
+        {blogposts.map((blogpost) => {
+          return (
+            <PortableText
+              key={blogpost._id}
+              value={blogpost.body}
+            />
+          )
+        })
+          
+        }
       </Box>
     </ThemeProvider>
   );
