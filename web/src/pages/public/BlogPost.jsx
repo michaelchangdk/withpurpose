5ef763388b7f3e3d50b7a7f3952f68bfe2766cf0
Amanda Tange <amandatange@hotmail.com>
2022-06-21 21:40:24 +0200

    deleted unused import

--- web/src/pages/public/BlogPost.jsx
+++ web/src/pages/public/BlogPost.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { client, urlFor } from "../../client";
-import { Helmet } from "react-helmet";
 import { PortableText } from "@portabletext/react";
 
 // MUI Imports
