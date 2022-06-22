b4ef5336d860dd5401ff6a646fcc6bad2e0003d7
michaelchangdk <michaelchangdk@icloud.com>
2022-06-22 10:51:26 +0200

    commented out access update on landing page while wpurpose does user tests today

--- web/src/pages/authenticated/LandingPage.jsx
+++ web/src/pages/authenticated/LandingPage.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
-import { client } from "../../client";
-import { useSelector, useDispatch } from "react-redux";
-import { authenticated } from "../../reducers/authenticated";
+import React from "react";
+// import { client } from "../../client";
+// import { useSelector, useDispatch } from "react-redux";
+// import { authenticated } from "../../reducers/authenticated";
 
 // MUI Imports
 import { Container } from "@mui/material";
@@ -18,31 +18,31 @@ import { FetchResponse } from "../../services/clientFunctions";
 // Query Declaration
 
 const LandingPage = () => {
-  const dispatch = useDispatch();
-  const userid = useSelector((store) => store.authenticated.uid);
+  // const dispatch = useDispatch();
+  // const userid = useSelector((store) => store.authenticated.uid);
   const pageQuery = `*[_type == "landingpage" && !(_id in path('drafts.**'))] {_id, landingpageelements[]-> {title, headline, description, slug, coverImage, _id}}`;
   const [loading, response] = FetchResponse(pageQuery);
 
-  const accessQuery = `*[_type == "user" && _id == "${userid}" && !(_id in path('drafts.**'))] {approvedCommunity, approvedMasterClass, approvedMentorBooking, approvedSchool, approvedWeek0, approvedWeek1, approvedWeek23, approvedWeek4, approvedWeek5, approvedWeek6}`;
-  useEffect(() => {
-    client.fetch(accessQuery).then((response) => {
-      dispatch(
-        authenticated.actions.updateAccess({
-          approvedSchool: response[0].approvedSchool,
-          approvedWeek0: response[0].approvedWeek0,
-          approvedWeek1: response[0].approvedWeek1,
-          approvedWeek23: response[0].approvedWeek23,
-          approvedWeek4: response[0].approvedWeek4,
-          approvedWeek5: response[0].approvedWeek5,
-          approvedWeek6: response[0].approvedWeek6,
-          approvedMasterClass: response[0].approvedMasterClass,
-          approvedMentorBooking: response[0].approvedMentorBooking,
-          approvedCommunity: response[0].approvedCommunity,
-        })
-      );
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // const accessQuery = `*[_type == "user" && _id == "${userid}" && !(_id in path('drafts.**'))] {approvedCommunity, approvedMasterClass, approvedMentorBooking, approvedSchool, approvedWeek0, approvedWeek1, approvedWeek23, approvedWeek4, approvedWeek5, approvedWeek6}`;
+  // useEffect(() => {
+  //   client.fetch(accessQuery).then((response) => {
+  //     dispatch(
+  //       authenticated.actions.updateAccess({
+  //         approvedSchool: response[0].approvedSchool,
+  //         approvedWeek0: response[0].approvedWeek0,
+  //         approvedWeek1: response[0].approvedWeek1,
+  //         approvedWeek23: response[0].approvedWeek23,
+  //         approvedWeek4: response[0].approvedWeek4,
+  //         approvedWeek5: response[0].approvedWeek5,
+  //         approvedWeek6: response[0].approvedWeek6,
+  //         approvedMasterClass: response[0].approvedMasterClass,
+  //         approvedMentorBooking: response[0].approvedMentorBooking,
+  //         approvedCommunity: response[0].approvedCommunity,
+  //       })
+  //     );
+  //   });
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   return (
     <BackgroundBox
